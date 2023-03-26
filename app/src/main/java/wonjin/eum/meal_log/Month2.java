45279044b0b25a2a43d047da63569486e505a4b9
Wonjin Eum <eumwonjin@gmail.com>
2023-03-27 06:55:10 +0900

    3/27 homework

--- app/src/main/java/wonjin/eum/meal_log/Month2.java
+++ app/src/main/java/wonjin/eum/meal_log/Month2.java
@@ -1,11 +1,17 @@
 package wonjin.eum.meal_log;
 
 import android.os.Bundle;
+import android.util.Log;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
 
 public class Month2 extends AppCompatActivity {
     @Override
@@ -17,6 +23,21 @@ public class Month2 extends AppCompatActivity {
         FirebaseFirestore db = FirebaseFirestore.getInstance();
 
         //read data , users 를 meal 로 바꾸기
+        db.collection("meal")
+                .get()
+                .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                    @Override
+                    public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                        if (task.isSuccessful()) {
+                            for (QueryDocumentSnapshot document : task.getResult()) {
+                                Log.d(TAG, document.getID()) + " => " + document.getData();
+                            }
+                        }
+                        else {
+                            Log.w(TAG, "Error getting documents.", task.getException());
+                        }
+                    }
+                });
 
     }
 }
--- app/src/main/java/wonjin/eum/meal_log/Stats.java
+++ app/src/main/java/wonjin/eum/meal_log/Stats.java
@@ -1,9 +1,21 @@
 package wonjin.eum.meal_log;
 
 import android.os.Bundle;
+import android.util.Log;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class Stats extends AppCompatActivity {
     @Override
@@ -11,7 +23,28 @@ public class Stats extends AppCompatActivity {
         super.onCreate(savedInstanceState);
 
         setContentView(R.layout.stats);
+        FirebaseFirestore db = FirebaseFirestore.getInstance();
 
         //add data
+
+        Map<String, Object> meal = new HashMap<>();
+        meal.put("breakfast", "");
+        meal.put("lunch", "");
+        meal.put("dinner", "");
+
+        db.collection("meal")
+                .add(meal)
+                .addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
+                    @Override
+                    public void onSuccess(DocumentReference documentReference) {
+                        Log.d(TAG, "DocumentSnapshot added with ID:" + documentReference.getId());
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Log.w(TAG, "Error adding document", e);
+                    }
+                });
     }
 }
