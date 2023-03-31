2e6c3710bac35da1e7f895998a0352a710aff650
Wonjin Eum <eumwonjin@gmail.com>
2023-03-31 18:30:09 +0900

    3/31 hw

--- app/src/main/java/wonjin/eum/meal_log/Menu.java
+++ app/src/main/java/wonjin/eum/meal_log/Menu.java
@@ -13,30 +13,38 @@ import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.QueryDocumentSnapshot;
 import com.google.firebase.firestore.QuerySnapshot;
 
+import android.annotation.SuppressLint;
+import android.os.Bundle;
+import android.util.Log;
+import android.webkit.WebResourceRequest;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
 public class Menu extends AppCompatActivity {
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
         setContentView(R.layout.menu);
 
-        FirebaseFirestore db = FirebaseFirestore.getInstance();
-
+        WebView myWebView = (WebView) findViewById(R.id.WebView);
+        myWebView.loadUrl("https://stevensonschool.flikisdining.com/menu/stevenson-school");
+        WebSettings webSettings = myWebView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+        myWebView.setWebViewClient(new WebViewClient());
         //read data
-        db.collection("users")
-                .get()
-                .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
-                    @Override
-                    public void onComplete(@NonNull Task<QuerySnapshot> task) {
-                        if (task.isSuccessful()) {
-                            for (QueryDocumentSnapshot document : task.getResult()) {
-                                Log.d("MENU >>>", document.getId() + " => " + document.getData());
-                            }
-                        } else {
-                            Log.w("MENU >>>", "Error getting documents.", task.getException());
-                        }
-                    }
-                });
+    }
+
+    private class WebViewClientClass extends WebViewClient {//페이지 이동
+        @Override
+        public boolean shouldOverrideUrlLoading(WebView view, WebResourceRequest wrr) {
+            Log.d("check URL", String.valueOf(wrr.getUrl()));
+            view.loadUrl(String.valueOf(wrr.getUrl()));
+            return true;
+        }
     }
 }
